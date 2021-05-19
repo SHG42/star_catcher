c70013ef713aaa53a1ba28feb8041f280345cdfc
SHG42 <shg.webdev@gmail.com>
2021-05-19 12:05:04 -0400

    last commit before adding touch controls, revert to here if need

--- public/scripts/sprites/Dude.js
+++ public/scripts/sprites/Dude.js
@@ -14,7 +14,7 @@ export default class Dude extends Phaser.Physics.Arcade.Sprite {
         this.scene.events.emit("dudeCreate");
         console.log("dude created in dude class");
     }
-
+//touch controls: swipe left or right to run, tap above sprite to jump
     update(cursors) {
         if (cursors.left.isDown) {
             this.setVelocityX(-160);
@@ -34,3 +34,24 @@ export default class Dude extends Phaser.Physics.Arcade.Sprite {
         }
     }
 }
+
+
+//KEYBOARD-ONLY CONTROLS
+// update(cursors) {
+//     if (cursors.left.isDown) {
+//         this.setVelocityX(-160);
+//         this.anims.play('left', true);
+//     } else if (cursors.right.isDown) {
+//         this.setVelocityX(160);
+//         this.anims.play('right', true);
+//     } else {
+//         this.setVelocityX(0);
+//         this.anims.play('turn');
+//     }
+    
+//     if (cursors.up.isDown && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+//         //second part of if statement prevents "walking/standing on air" while up key is held down, can doublejump but up key has to be pressed again to jump again, otherwise dude would stay at y-coord as long as up key is held down
+//         //add  && this.dude.body.onFloor() into if statement if we want to disable doublejump
+//         this.setVelocityY(-330);
+//     }
+// }
